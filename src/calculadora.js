0fb20f0e858be7846de1d31e6c27c4eb2db80f30
MariaBracho <mariabrachovillanueva@gmail.com>
2021-09-10 19:21:46 -0400

    save calculator

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,1 +1,28 @@
-export class c
+let value=40+7
+let change=`${value}`
+
+class Calculator{
+	constructor(input){
+		this.result=0
+		this.input=0
+		this.inputString=`${this.input}`
+	}
+	getInput(input){
+		this.input=input
+	}
+	getOperacion(operacion){
+		this.result=operacion
+	}
+	getResult(){
+	   return this
+	}
+}
+
+const cal=new Calculator()
+cal.getInput(value)
+cal.getOperacion(change)
+
+console.log(cal.getResult())
+console.log("esto funcion?")
+
+
